25f3c021c2d9623ad33604cfbc5376e9d90836ab
Kival <fa.vakurov@gmail.com>
2025-01-25 18:09:26 +0500

    добавлена связь фронта расписания с бэком

--- front/src/app/(user)/stats/page.tsx
+++ front/src/app/(user)/stats/page.tsx
@@ -1,15 +1,89 @@
-import { BarChartStats } from '@/widgets/bar-chart'
+
 import { Metadata } from 'next'
+import { cookies } from 'next/headers'
+import { Bar, BarChart, CartesianGrid, Cell, XAxis, YAxis } from 'recharts'
+
+import {
+	Card,
+	ChartConfig,
+	ChartContainer,
+	ChartTooltip,
+	ChartTooltipContent,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle
+} from '@/shared/ui'
 import React from 'react'
 
 export const metadata: Metadata = {
 	title: 'Статистика'
 }
 
-export default function Stats() {
+interface GradeData {
+	id: number;
+	name: string;
+	average_grade: string; // Поскольку в вашем примере это строка
+  }
+
+export const revalidate = 60
+
+const chartData = Array.from({ length: 20 }, (_, i) => ({
+	element: i + 1,
+	assessment: Math.random() * 5 + 1
+}))
+
+const chartConfig = {
+	assessment: {
+		label: 'Средняя оценка: ',
+		color: 'hsl(var(--chart-1))'
+	}
+} satisfies ChartConfig
+
+export default async function Stats() {
+	const cookieStore = await cookies()
+  	const token = cookieStore.get('token')?.value
+
+	const { data } = await fetch(`${process.env.SERVER_URL}/grades/average`, {
+		method: 'GET',
+		headers: {
+			'Authorization': `Bearer ${token}`,
+		}
+	})
+		.then((res) => res.json())
+		.catch((error) => console.error(error))
+
+	
+	// const chartData = data.group_average_grades
+	// console.log(chartData)
+	// const chartData = Object.values(data?.group_average_grades || {})
+
+	// console.log(chartData)
+
+
 	return (
-		<div className='w-[50%] h-[50%]'>
-			<BarChartStats />
-		</div>
+		<Card>
+			<CardHeader>
+				<CardTitle>Итоговый рейтинг за год</CardTitle>
+				<CardDescription>
+					{new Date().getFullYear()} - {new Date().getFullYear() + 1}
+				</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<ChartContainer config={chartConfig}>
+					<BarChart accessibilityLayer data={chartData}>
+						<CartesianGrid vertical={false} />
+						<YAxis tickLine={false} domain={[0, 5]} ticks={[0, 1, 2, 3, 4, 5]} axisLine={false} tickMargin={20} />
+						<XAxis dataKey='id' tickLine={false} axisLine={false} tickMargin={20} />
+						<ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />
+						<Bar dataKey='average_grade' radius={8}>
+							{chartData?.map((_, index) => (
+								<Cell cursor='pointer' fill={index === 0 ? '#82ca9d' : 'hsl(var(--chart-1))'} key={`cell-${index}`} />
+							))}
+						</Bar>
+					</BarChart>
+				</ChartContainer>
+			</CardContent>
+		</Card>
 	)
 }
--- front/src/widgets/bar-chart/ui.tsx
+++ front/src/widgets/bar-chart/ui.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { TrendingUp } from 'lucide-react'
 import { Bar, BarChart, CartesianGrid, Cell, XAxis, YAxis } from 'recharts'
 
@@ -16,42 +14,9 @@ import {
 	CardTitle
 } from '@/shared/ui'
 
-const chartData = Array.from({ length: 20 }, (_, i) => ({
-	element: i + 1,
-	assessment: Math.random() * 5 + 1
-}))
 
-const chartConfig = {
-	assessment: {
-		label: 'Средняя оценка: ',
-		color: 'hsl(var(--chart-1))'
-	}
-} satisfies ChartConfig
+export const revalidate = 60
 
-export function BarChartStats() {
-	return (
-		<Card>
-			<CardHeader>
-				<CardTitle>Итоговый рейтинг за год</CardTitle>
-				<CardDescription>
-					{new Date().getFullYear()} - {new Date().getFullYear() + 1}
-				</CardDescription>
-			</CardHeader>
-			<CardContent>
-				<ChartContainer config={chartConfig}>
-					<BarChart accessibilityLayer data={chartData}>
-						<CartesianGrid vertical={false} />
-						<YAxis tickLine={false} domain={[0, 5]} ticks={[0, 1, 2, 3, 4, 5]} axisLine={false} tickMargin={20} />
-						<XAxis dataKey='element' tickLine={false} axisLine={false} tickMargin={20} />
-						<ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />
-						<Bar dataKey='assessment' radius={8}>
-							{chartData.map((_, index) => (
-								<Cell cursor='pointer' fill={index === 0 ? '#82ca9d' : 'var(--color-desktop)'} key={`cell-${index}`} />
-							))}
-						</Bar>
-					</BarChart>
-				</ChartContainer>
-			</CardContent>
-		</Card>
-	)
+export default async function BarChartStats() {
+	
 }
